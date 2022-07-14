fcb14eaee4088dd1c112538ce82b13bfbb5520bc
kabinny <kabinny@gmail.com>
2022-07-15 07:44:39 +0900

    복수 숫자 입력

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,34 @@ import "./style.css";
 
 const VALID_NUMBER_OF_DIGITS = 3;
 const BASE_DIGIT = 10;
+const INIT_VALUE = 0
 
+const validateNumberLength = (value: string | number) => {
+  return String(value).length < VALID_NUMBER_OF_DIGITS
+}
 
-// resultEl.addEventListener("click", function ({ target }: MouseEvent) {
-//   if (target) {
-//     alert((target as HTMLDivElement).innerText)
-//   }
-// })
+const isZero = (value: string) => Number(value) === 0
 
 const Calculator = {
   value: 0,
-  render() {
+  render(inputValue: string | number) {
     const resultEl = <HTMLDivElement>document.querySelector("#result")
+    const prevValue = resultEl.innerText
+
+    if (!validateNumberLength(prevValue)) {
+      alert("3자리 초과 숫자를 입력할 수 없습니다.")
+      return
+    }
 
     if (resultEl) {
-      resultEl.innerText = String(this.value)
+      resultEl.innerText = isZero(prevValue) ? String(inputValue) : String(prevValue + inputValue)
     }
   },
   reset() {
     this.value = 0
+  },
+  operator(inputNumber: number) {
+    this.render(inputNumber)
   },
   initEvent() {
     const buttonContainerEl = document.querySelector(".contents")
@@ -31,10 +40,11 @@ const Calculator = {
       if (buttonText === "AC") {
         this.reset()
       } else {
+        this.render(Number(buttonText))
       }
     })
   },
 }
 
-Calculator.render()
+Calculator.render(INIT_VALUE)
 Calculator.initEvent()
