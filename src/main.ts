ee0b7bf1d9f483d59d9ca08b9c2b252a0db05a36
kabinny <kabinny@gmail.com>
2022-07-14 19:19:03 +0900

    HTML 요소 타이핑

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,10 @@ import "./style.css";
 
 const VALID_NUMBER_OF_DIGITS = 3;
 const BASE_DIGIT = 10;
+
+// const resultEl = document.querySelector("#result") as HTMLElement
+const resultEl = <HTMLDivElement>document.querySelector("#result") 
+
+if (resultEl) {
+  resultEl.innerText = String(99999)
+}
