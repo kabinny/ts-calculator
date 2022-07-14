d1c5ec26d6e1c90e6ec5ec43b78ed214ca960193
kabinny <kabinny@gmail.com>
2022-07-14 21:29:32 +0900

    계산기 렌더링 구현

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,38 @@ import "./style.css";
 const VALID_NUMBER_OF_DIGITS = 3;
 const BASE_DIGIT = 10;
 
-// const resultEl = document.querySelector("#result") as HTMLElement
-const resultEl = <HTMLDivElement>document.querySelector("#result") 
 
-if (resultEl) {
-  resultEl.innerText = String(99999)
+// resultEl.addEventListener("click", function ({ target }: MouseEvent) {
+//   if (target) {
+//     alert((target as HTMLDivElement).innerText)
+//   }
+// })
+
+const Calculator = {
+  value: 0,
+  render() {
+    const resultEl = <HTMLDivElement>document.querySelector("#result")
+
+    if (resultEl) {
+      resultEl.innerText = String(this.value)
+    }
+  },
+  reset() {
+    this.value = 0,
+  },
+  initEvent() {
+    const buttonContainerEl = document.querySelector(".contents")
+
+    buttonContainerEl?.addEventListener("click", function ({ target }) {
+      const buttonText = (target as HTMLButtonElement).innerText
+
+      if (buttonText === "AC") {
+        this.reset()
+      } else {
+      }
+    })
+  },
 }
 
-resultEl.addEventListener("click", function ({ target }: MouseEvent) {
-  if (target) {
-    alert((target as HTMLDivElement).innerText)
-  }
-})
+Calculator.render()
+Calculator.initEvent()
